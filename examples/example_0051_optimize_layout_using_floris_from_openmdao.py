12db937171b5ac6620218e7e0e789c2fe14d0864
jaredthomas68 <jaredthomas68@gmail.com>
2019-11-07 12:46:59 -0700

    complete layout  optimizaiton example using floris from openmdao

--- examples/example_0051_optimize_layout_using_floris_from_openmdao.py
+++ examples/example_0051_optimize_layout_using_floris_from_openmdao.py
@@ -26,22 +26,27 @@ FlorisComponent = fi._floris_openMDAO()
 # set up OpenMDAO problem
 prob = om.Problem()
 
+# set up indep var comp for optimization design variables
+indeps = prob.model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
+indeps.add_output('turbineX', fi.layout_x, units='m')
+indeps.add_output('turbineY', fi.layout_y, units='m')
+
 # add floris to the OpenMDAO problem
 prob.model.add_subsystem('floris', FlorisComponent(fi=fi), promotes=['*'])
+
+# set up objective component
+obj_comp = om.ExecComp('obj = -AEP')
+prob.model.add_subsystem('obj_comp', obj_comp, promotes=['obj', 'AEP'])
 
 # setup the optimizer
 prob.driver = om.ScipyOptimizeDriver()
 prob.driver.options['optimizer'] = 'SLSQP'
 prob.driver.options['tol'] = 1e-6
 
-# set up design variables
-indeps = prob.model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
-indeps.add_output('turbineX', fi.layout_x)
-indeps.add_output('turbineY', fi.layout_y)
-
-prob.model.add_design_var('turbineX', lower=np.min(fi.layout_x), upper=np.max(fi.layout_x))
-prob.model.add_design_var('turbineY', lower=np.min(fi.layout_y), upper=np.max(fi.layout_y))
-prob.model.add_objective('AEP')
+# set up design variable
+prob.model.add_design_var('turbineX', lower=np.min(fi.layout_x-100), upper=np.max(fi.layout_x+100))
+prob.model.add_design_var('turbineY', lower=np.min(fi.layout_y-100), upper=np.max(fi.layout_y+100))
+prob.model.add_objective('obj', scaler=1E-10)
 
 # Ask OpenMDAO to finite-difference across the model to compute the gradients for the optimizer
 prob.model.approx_totals()
@@ -49,19 +54,19 @@ prob.model.approx_totals()
 # setup model
 prob.setup()
 
-# run optimization problem
-prob.run_driver()
-
-# set up OpenMDAO problem
-prob.setup()
-
 # define wind rose information
 prob['wind_direction'] = 270.
 prob['wind_frequency'] = 1.
 prob['wind_speed'] = 8.0
 
-# Calculate AEP
+# get initial aep value
 prob.run_model()
+aep_init = np.copy(prob['AEP'])
+
+# run optimization problem
+prob.run_driver()
 
 # Print AEP
-print('AEP : ', prob['AEP'])
+print('Initial AEP: ', aep_init)
+print('Final AEP : ', prob['AEP'])
+print('Percent improvement: ', 100.*((prob['AEP']/aep_init)-1.))
