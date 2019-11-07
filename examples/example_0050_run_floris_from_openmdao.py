e12e004fce41b12519a500c9326ac756be169081
jaredthomas68 <jaredthomas68@gmail.com>
2019-11-07 11:23:41 -0700

    add example for calling floris from openmdao

---
+++ examples/example_0050_run_floris_from_openmdao.py
@@ -0,0 +1,41 @@
+#
+# Copyright 2019 NREL
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+# Try out calling floris from OpenMDAO
+
+import numpy as np
+import floris.tools as wfct
+import openmdao.api as om
+
+# Initialize the FLORIS OpenMDAO interface
+fi = wfct.floris_utilities.FlorisInterface("example_input.json", mode='python', openmdao=True)
+
+# get OpenMDAO component
+FlorisComponent = fi._floris_openMDAO()
+
+# set up OpenMDAO problem
+prob = om.Problem()
+
+# add floris to the OpenMDAO problem
+prob.model.add_subsystem('floris', FlorisComponent(fi=fi))
+
+# define wind rose information
+prob['wind_direction'] = 270.
+prob['wind_frequency'] = 1.
+prob['wind_speed'] = 8.0
+
+# Calculate AEP
+prob.run_model()
+
+# Print AEP
+print('AEP : ', prob['AEP'])
